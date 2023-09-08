e07f1407c11b0c09c055e64f89c96a7e387b0ab7
SantiKnight <santiknight2003@gmail.com>
2023-09-08 13:17:28 -0300

    eliminated uselss files and added pandas logic to main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import read_csv
 import charts
 import utils
+import pandas as pd
 
 def run():
-    data = read_csv.read_csv('./data.csv')
+    '''
     data = list(filter(lambda x: x['Continent'] == 'South America', data))
-    
     countries = list(map(lambda x: x['Country/Territory'], data))
     porcentages = list(map(lambda x: x['World Population Percentage'], data))
+    '''
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']
+    
+    countries = df['Country/Territory'].values
+    porcentages = df['World Population Percentage'].values
+    
     charts.generate_pie_chart(countries, porcentages)
     
+    data = read_csv.read_csv('./data.csv')
     country = input("Ingrese el pais => ")
     result = utils.get_population_by_country(data, country)
     
